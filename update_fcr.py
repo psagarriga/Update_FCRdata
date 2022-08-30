a44e6f5ccd53fef1899ca04a8049a37cdd5a9167
Pierluigi Sagarriga Visconti <112080082+psagarriga@users.noreply.github.com>
2022-08-30 16:20:27 +0200

    Update update_fcr.py

--- update_fcr.py
+++ update_fcr.py
@@ -2,7 +2,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import dataframe_image as dfi
+
 
 ######################### TOKEN #########################
 token = os.environ.get("MY_SECRET_TOKEN")
