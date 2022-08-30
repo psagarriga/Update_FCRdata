ba9ca5501b766ed4ac6840ae600d2200b141ad66
Pierluigi Sagarriga Visconti <112080082+psagarriga@users.noreply.github.com>
2022-08-30 16:06:04 +0200

    Update python with Table png

--- update_fcr.py
+++ update_fcr.py
@@ -4,87 +4,86 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
+######################### TOKEN #########################
 token = os.environ.get("MY_SECRET_TOKEN")
 if not token:
   raise RuntimeError("MY_SECRET_TOKEN env var is not set!")
 print("All good! we found our env var")
 
-
-
-
+######################### READ DATA FCR #########################
 
 data = pd.read_csv("https://bit.ly/3wrY3lV", sep=";")
-print(data.info())
 
 
-
-
-######################### CONCATENATE DATE & TIME COLUMNS (two methods, but diffferent dtype) v#########################
-# https://stackoverflow.com/questions/17978092/combine-date-and-time-columns-using-python-pandas
-
-# Method 1 (dtype: object)
-# Datetime = data['Date'] + " " + data['Time']                          #dtype: object
-# Method 2 (dtype: datetime64[ns])
+######################### CONCATENATE DATE & TIME COLUMNS #########################
 data['Datetime'] = pd.to_datetime(data['Date'] + " " + data['Time'])    #dtype: datetime64[ns]
 
-data["Unnamed: 5"] = data["Price"] * 2
-data.rename({'Value': 'Demand', 'Price': 'Price0,5h', 'Unnamed: 5': 'Price1h'}, axis=1, inplace=True)
-data.drop(['Price0,5h', 'Direction'], axis=1, inplace=True)
+data["Price1h"] = data["Price"] * 2 #"Price" is the 30min Price
+data.rename({'Value': 'Demand'}, axis=1, inplace=True)
+data.drop(['Price', 'Unnamed: 5','Direction'], axis=1, inplace=True)
 
+data['Datetime_Index']=data['Datetime']
+data.set_index('Datetime_Index', inplace=True)
 
-
-######################### SAVE NEW TABLE AS A CSV ######################### 
-
-data.to_csv("_export/dataLW.csv")
-
-
-print("This is a test1")
-
-
-######################### SIMPLE PLOT GRAPH #########################
-
-plt.plot(data['Price1h'])
-#plt.axis([0, 30000, 0, 520])
-plt.ylabel('Price [€/MW per h]')
-plt.show()
+data1D = data.last('1D')
+data90D = data.last('90D')
 
 
 ######################### Create HTML Interactiv Graph for website #########################
-#########################  figure with secondary y-axis            #########################
 
+ 
+########################  FCR Price Graph            #########################
 
-fig = go.Figure()
-fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-fig.add_trace(go.Scatter(x=data["Datetime"],y=data["Price1h"],name="Price1h"), secondary_y=False,)
-fig.add_trace(go.Scatter(x=data["Datetime"],y=data["Demand"],name="Demand"), secondary_y=True,)
-fig.update_layout(hovermode="x unified")
-fig.update_layout(
- #   title="FCR Interactive Graph",
- #   xaxis_title="Time (h)",
- #   yaxis_title="FCR Price [€/MW per h]",
+fig1 = go.Figure()
+fig1.add_trace(go.Scatter(x=data["Datetime"],y=data["Price1h"],name="Price1h"))
+fig1.update_layout(hovermode="x unified")
+fig1.update_layout(
+    title="FCR Settlement Price",
+    xaxis_title="Time (h)",
+    yaxis_title="FCR Settlement Price [€/MW per h]",
     legend=dict(
     yanchor="top",
     y=0.99,
     xanchor="left",
     x=0.01))
 
-fig.update_yaxes(title_text="<b>Price</b> [€/MW per h]", secondary_y=False, range=[0,400])
-fig.update_yaxes(title_text="<b>Total demand</b> [MW]", secondary_y=True, range=[0,700])
-fig.show()
+fig1.show()
 
- 
 
+#########################  FCR Price Graph 90D            #########################
+
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=data90D["Datetime"],y=data90D["Price1h"],name="Price1h"))
+fig2.update_layout(hovermode="x unified")
+fig2.update_layout(
+    title="FCR Settlement Price last 90 days",
+    xaxis_title="Time (h)",
+    yaxis_title="FCR Settlement Price [€/MW per h]",
+    legend=dict(
+    yanchor="top",
+    y=0.99,
+    xanchor="left",
+    x=0.01))
+
+fig2.show()
+  
+  
 ######################### SAVE HTML Interactiv Graph for website #########################
-fig.write_html("graph_FCR.html")
 
-######################### Create table with summary of information #########################
-######################### create dataframe with describe.          #########################
 
-data.describe().index
-data.describe().columns
-dd = data.describe()
+fig1.write_html("graph_FCR_price.html")
+fig2.write_html("graph_FCR_price_90D.html")
 
-######################### Save table with summary of information #########################
-dd.to_html("table_FCR.html")
+  
+    
+######################### SAVE Table as png                   #########################
+  
+  
+data1D.reset_index(inplace=True)
+data1Db = data1D[['Date', 'Time', 'Price1h']]
+
+data1Db_Style = data1Db.style.background_gradient(axis=0, gmap=data1Db['Price1h'], cmap='rainbow')
+dfi.export(data1Db_Style, "data1D.png")
+  
+    
+  
