81f1ed65c11bc7a9b3664312148fdf64340c7e5d
Pierluigi Sagarriga Visconti <112080082+psagarriga@users.noreply.github.com>
2022-08-30 17:02:21 +0200

    Update update_fcr.py

--- update_fcr.py
+++ update_fcr.py
@@ -4,6 +4,17 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+df = pd.DataFrame([[38.0, 2.0, 18.0, 22.0, 21, np.nan],[19, 439, 6, 452, 226,232]],
+                  index=pd.Index(['Tumour (Positive)', 'Non-Tumour (Negative)'], name='Actual Label:'),
+                  columns=pd.MultiIndex.from_product([['Decision Tree', 'Regression', 'Random'],['Tumour', 'Non-Tumour']], names=['Model:', 'Predicted:']))
+df.style
+
+
+
 ######################### TOKEN #########################
 token = os.environ.get("MY_SECRET_TOKEN")
 if not token:
