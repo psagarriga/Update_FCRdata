e6e24668b644872a8b7e6b376a1467d56ce0d3a6
Pierluigi Sagarriga Visconti <112080082+psagarriga@users.noreply.github.com>
2022-08-30 19:55:31 +0200

    Update update_fcr.py

--- update_fcr.py
+++ update_fcr.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import numpy as np
-import df2img
-from df2img import df2img
+#import df2img
+#from df2img import df2img
 #from plotly.subplots import make_subplots
 #import matplotlib as mpl
 
